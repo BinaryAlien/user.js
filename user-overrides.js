f0f1b9b1ecd3449289e14262a3051de142cad459
Binary <49949198+BinaryAlien@users.noreply.github.com>
2024-03-26 22:28:44 +0000

    overrides

---
+++ user-overrides.js
@@ -0,0 +1,8 @@
+/* 4501 */
+user_pref("privacy.resistFingerprinting", false);
+
+/* 4504 */
+user_pref("privacy.resistFingerprinting.letterboxing", false);
+
+/* 4510 */
+user_pref("browser.display.use_system_colors", true);
